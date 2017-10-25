b81d8e55ac447f9dfc1ecf7e31467c2ab37add5b
Ian F <ianmichael.fennie@haw-hamburg.de>
2017-10-25 11:12:37 +0200

    Write & test Tree.postorderTraversal()

--- src/Tree.java
+++ src/Tree.java
@@ -1,3 +1,5 @@
+import java.io.*;
+
 /**
  * ADLab02
  * Created by IF on 25.10.17.
@@ -23,14 +25,16 @@ public class Tree {
      * @param node BiNode to begin traversing
      */
     private void postorderTraversal(BiNode node) {
-
+        if (node.leftChild != null) { postorderTraversal(node.leftChild); }
+        if (node.rightChild != null) { postorderTraversal(node.rightChild); }
+        System.out.println(node.string);
     }
 
     /**
      * Recursive Tree traversal method using the Post-order traversal sequence
      */
     private void postorderTraversal() {
-
+        postorderTraversal(rootNode);
     }
 
     /**
@@ -38,12 +42,15 @@ public class Tree {
      * @param args String array of arguments
      */
     public static void main(String[] args) {
-        // Testing Tree creation
+        // Tree creation Test
         BiNode three = new BiNode("3");
         BiNode four = new BiNode("4");
         BiNode five = new BiNode("5");
         BiNode plus = new BiNode("+", four, three);
         BiNode root = new BiNode("*", five, plus);
         Tree tree = new Tree(root);
+
+        // Post-order tree traversal Test
+        tree.postorderTraversal();
     }
 }
