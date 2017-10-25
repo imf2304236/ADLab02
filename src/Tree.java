4ab8dcdb220970df44f278c69b270186a2e55fe7
Ian F <ianmichael.fennie@haw-hamburg.de>
2017-10-25 10:59:34 +0200

    Add javadoc comments to Tree * Fix typo in Tree.postorderTraversal()

--- src/Tree.java
+++ src/Tree.java
@@ -7,19 +7,36 @@ public class Tree {
     private BiNode rootNode;
 
     // Constructor
+
+    /**
+     * Tree constructor method
+     * @param root BiNode to be assigned as the root node of the Tree
+     */
     public Tree(BiNode root) {
         rootNode = root;
     }
 
     // Methods
+
+    /**
+     * Recursive Tree traversal method using the Post-order traversal sequence
+     * @param node BiNode to begin traversing
+     */
     private void postorderTraversal(BiNode node) {
 
     }
 
-    private void prostorderTraversal() {
+    /**
+     *
+     */
+    private void postorderTraversal() {
 
     }
 
+    /**
+     * Main test method
+     * @param args String array of arguments
+     */
     public static void main(String[] args) {
         // Testing Tree creation
         BiNode three = new BiNode("3");
