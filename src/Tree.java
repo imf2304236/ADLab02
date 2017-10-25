b61e7af6cf6126f3d0ce2572d174ad959c977235
Ian F <ianmichael.fennie@haw-hamburg.de>
2017-10-25 10:44:13 +0200

    Encapsulate testTree in Tree.main()

--- src/Tree.java
+++ src/Tree.java
@@ -10,4 +10,12 @@ public class Tree {
     public Tree(BiNode root) {
         rootNode = root;
     }
+
+    public static void main(String[] args) {
+        // Testing Tree creation
+        BiNode leftNode = new BiNode("left");
+        BiNode rightNode = new BiNode("right");
+        BiNode rootNode = new BiNode("root", leftNode, rightNode);
+        Tree testTree = new Tree(rootNode);
+    }
 }
--- src/test/TreeTest.java
+++
@@ -1,9 +0,0 @@
-import static org.junit.jupiter.api.Assertions.*;
-
-/**
- * ADLab02
- * Created by IF on 25.10.17.
- */
-class TreeTest {
-
-}
