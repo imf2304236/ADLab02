dc70d7be80231b7c1729d160ff3267b015a41626
Ian F <ianmichael.fennie@haw-hamburg.de>
2017-10-25 12:42:20 +0200

    * Write & test Tree.construct(String) * Add Tree(String) constructor

--- src/Tree.java
+++ src/Tree.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 /**
  * ADLab02
@@ -16,6 +18,14 @@ public class Tree {
      */
     public Tree(BiNode root) {
         rootNode = root;
+    }
+
+    /**
+     *
+     * @param postfix
+     */
+    public Tree(String postfix) {
+        rootNode = this.construct(postfix);
     }
 
     // Methods
@@ -38,6 +48,33 @@ public class Tree {
     }
 
     /**
+     * Postfix conversion method which builds a binary tree structure
+     * @param postfix
+     * @return Binode which represents the root of the tree structure
+     */
+    private BiNode construct(String postfix) {
+        Stack<BiNode> stack = new Stack<>();
+        StringTokenizer tokenizer = new StringTokenizer(postfix);
+        double d;
+
+        while (tokenizer.hasMoreTokens()) {
+            String token = tokenizer.nextToken();
+
+            try {
+                d = Double.parseDouble(token);
+                BiNode node = new BiNode(token);
+                stack.push(node);
+            } catch (NumberFormatException e) {
+                BiNode rightNode = stack.pop();
+                BiNode leftNode = stack.pop();
+                BiNode node = new BiNode(token, leftNode, rightNode);
+                stack.push(node);
+            }
+        }
+        return stack.pop();
+    }
+
+    /**
      * Main test method
      * @param args String array of arguments
      */
@@ -52,5 +89,9 @@ public class Tree {
 
         // Post-order tree traversal Test
         tree.postorderTraversal();
+
+        // Tree construction Test
+        String postfix = "5 4 3 + *";
+        Tree treeConstruct = new Tree(postfix);
     }
 }
